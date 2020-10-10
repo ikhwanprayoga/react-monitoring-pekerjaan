8552dd867b1312686b2c1e9a3844f2eefa3f0893
ikhwansiskom14 <dewa.siskom@gmail.com>
2020-10-10 14:25:46 +0700

    Updates

---
+++ precache-manifest.b5fcf1f19fb41e172e2a4978787f1c53.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8438d5ac50f362c78566a8d0dd596ff5",
+    "url": "/react-monitoring-pekerjaan/index.html"
+  },
+  {
+    "revision": "1777a71237f2e09f2b27",
+    "url": "/react-monitoring-pekerjaan/static/css/2.f06db823.chunk.css"
+  },
+  {
+    "revision": "03efd7a85b21867104c8",
+    "url": "/react-monitoring-pekerjaan/static/css/main.37507891.chunk.css"
+  },
+  {
+    "revision": "1777a71237f2e09f2b27",
+    "url": "/react-monitoring-pekerjaan/static/js/2.6f889574.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-monitoring-pekerjaan/static/js/2.6f889574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03efd7a85b21867104c8",
+    "url": "/react-monitoring-pekerjaan/static/js/main.c23dfb87.chunk.js"
+  },
+  {
+    "revision": "e315e9d4e08981c2e794",
+    "url": "/react-monitoring-pekerjaan/static/js/runtime-main.91937ea8.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/react-monitoring-pekerjaan/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/react-monitoring-pekerjaan/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/react-monitoring-pekerjaan/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/react-monitoring-pekerjaan/static/media/notification.c0d3c94c.eot"
+  }
+]);
